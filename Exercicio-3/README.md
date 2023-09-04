4ca07e26749090a235c99a2266200d1bdb2efa55
Gueiros <rodolfo.gueiros@accenture.com>
2023-09-03 23:13:40 -0300

    Subindo o programa calculando salario de funcionario

---
+++ Exercicio-3/README.md
@@ -0,0 +1,4 @@
+# Exercicios-3
+Essa lista de exercícios segue o conteúdo do curso de [Nelio Alves na Udemy.](https://www.udemy.com/course/java-curso-completo)
+
+<img src="https://github.com/RodolfoGueiros/Exercicios-Java/blob/main/Exercicio-3/exercicio-3.png?raw=true">
---
+++ Exercicio-3/src/entities/Employee.java
@@ -0,0 +1,25 @@
+package entities;
+
+public class Employee {
+    public String name;
+    public double grossSalary; //salario bruto
+    public double tax; //imposto
+
+    //salario liquido
+    public double netSalary(){
+
+        return grossSalary - tax;
+    }
+
+    //incremento de salario
+    public void increaseSalary(double percentage){
+
+        grossSalary += grossSalary * percentage / 100;
+    }
+
+    public String toString() {
+        return name
+                + ", $ "
+                + String.format("%.2f", netSalary());
+    }
+}

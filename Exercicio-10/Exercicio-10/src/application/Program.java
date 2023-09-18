8858cb59491341020008e5706bfd16ff4393574b
Gueiros <rodolfo.gueiros@accenture.com>
2023-09-17 22:53:40 -0300

    Exercicio 10 - Calculo de media usando tipo referencia ponteiro

---
+++ Exercicio-10/Exercicio-10/src/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Product[] vect = new Product[n];
+
+        //Calculando média usando array usando ponteiro
+        for (int i=0; i<vect.length; i++){
+            sc.nextLine();
+            String name = sc.nextLine();
+            double price = sc.nextDouble();
+            vect[i] = new Product(name, price);
+        }
+        double sum = 0;
+        for (int i=0; i<vect.length; i++){
+            sum += vect[i].getPrice();
+        }
+        double avg = sum / vect.length;
+
+        System.out.println("Average " + avg);
+    }
+}
